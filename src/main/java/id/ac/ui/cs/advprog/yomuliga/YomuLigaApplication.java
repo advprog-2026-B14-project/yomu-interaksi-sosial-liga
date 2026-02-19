e34dee77d23da84cc0edb3ac5b20164ec1a9b13b
vd <mrafighalibf@gmail.com>
2026-02-20 00:35:03 +0700

    chore: initialize project

---
+++ src/main/java/id/ac/ui/cs/advprog/yomuliga/YomuLigaApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomuliga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YomuLigaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YomuLigaApplication.class, args);
+    }
+
+}
