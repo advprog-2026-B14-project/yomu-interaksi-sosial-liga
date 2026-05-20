949f59fac9f37750dc3b8e057e16e818a18aad52
aleenya <anya.aleenaa@gmail.com>
2026-05-21 01:21:27 +0700

    test: fix contextLoads failure by adding mock properties

--- src/test/java/id/ac/ui/cs/advprog/yomuliga/YomuLigaApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/yomuliga/YomuLigaApplicationTests.java
@@ -3,7 +3,13 @@ package id.ac.ui.cs.advprog.yomuliga;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "achievement.service.base-url=http://localhost:8083",
+        "achievement.service.daily-mission-path=/api/dummy/path",
+        "learning.service.base-url=http://localhost:8081",
+        "learning.service.token=dummy-token",
+        "learning.service.stats-path=/api/dummy/stats"
+})
 class YomuLigaApplicationTests {
 
     @Test
