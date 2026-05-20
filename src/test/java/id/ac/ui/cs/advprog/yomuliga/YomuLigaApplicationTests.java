01332d30c82f74279470ffdb149f3c0c9fa33f00
aleenya <anya.aleenaa@gmail.com>
2026-05-21 01:29:42 +0700

    test: disable database autoconfiguration for contextLoads

--- src/test/java/id/ac/ui/cs/advprog/yomuliga/YomuLigaApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/yomuliga/YomuLigaApplicationTests.java
@@ -1,6 +1,9 @@
 package id.ac.ui.cs.advprog.yomuliga;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
@@ -9,6 +12,11 @@ import org.springframework.boot.test.context.SpringBootTest;
         "learning.service.base-url=http://localhost:8081",
         "learning.service.token=dummy-token",
         "learning.service.stats-path=/api/dummy/stats"
+})
+
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
 })
 class YomuLigaApplicationTests {
 
